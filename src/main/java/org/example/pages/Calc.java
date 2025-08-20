293925df6eeaa819fa4b35a5a1f0ccb4cb7ad5fa
S.chernyshev <s.chernyshev@lantan.info>
2025-08-20 14:57:19 +0700

    New branch calculator test

---
+++ src/main/java/org/example/pages/Calc.java
@@ -0,0 +1,10 @@
+package org.example.pages;
+
+public class Calc {
+    public int summ(int a, int b) {
+        int result;
+        result = a + b;
+        System.out.printf("Сумма %d + %d = %d", a, b, result);
+        return result;
+    }
+}
